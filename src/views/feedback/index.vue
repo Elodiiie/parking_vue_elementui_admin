8ba13fc8f549dbb953a704c883c74f7d0a4701cb
Elodiiie <glx983793727@163.com>
2021-11-18 23:27:10 +0800

    “”

---
+++ src/views/feedback/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="app-container documentation-container" />
+</template>
+
+<script>
+
+export default {
+  name: 'FinanceDocu',
+  data() {
+    return {
+      articleList: [
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.documentation-container {
+  margin: 100px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  .document-btn {
+    flex-shrink: 0;
+    display: block;
+    cursor: pointer;
+    background: black;
+    color: white;
+    height: 60px;
+    padding: 0 16px;
+    margin: 16px;
+    line-height: 60px;
+    font-size: 20px;
+    text-align: center;
+  }
+
+}
+</style>
